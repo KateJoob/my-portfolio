261dc2535fe78ad310fd8ed4c9db1c7093ee8b7c
KateJoob <66968414+KateJoob@users.noreply.github.com>
2024-08-29 01:34:52 +0000

    ddd

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Sawasdee Kate..');
+  res.send('Sawasdee Kate...');
 });
 
 const PORT = 5000;
